fe00f91294cf604cb84654fda120e262ceac9877
Lucas Opoka <lucasopoka@icloud.com>
2024-12-05 12:55:30 +0200

    Day 5

---
+++ day5/day5_2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+
+
+int main(void)
+{
+	std::ifstream inputFile("./input.txt");
+	std::string line;
+	std::regex ptrn1("([0-9]{2})\\|([0-9]{2})");
+	std::regex ptrn2("(\\d+)(,\\d+)+");
+	int res = 0;
+	std::map<int, std::vector<int>> map;
+
+	while (std::getline(inputFile, line))
+	{
+		for (std::sregex_iterator itr = std::sregex_iterator(line.begin(), line.end(), ptrn1); itr != std::sregex_iterator(); itr++)
+			map[std::stoi(itr->str(1))].push_back(std::stoi(itr->str(2)));
+		
+		if (std::regex_match(line, ptrn2))
+		{
+			std::stringstream ss(line);
+			std::vector<int> vec;
+			char sep;
+			int tmp;
+			bool cond = true;
+
+			while (ss >> tmp)
+			{
+				vec.push_back(tmp);
+				ss >> sep;
+			}
+		
+			for (int i = 0; i < vec.size(); i++)
+				for (int j = 0; j < i; j++)
+					cond = cond ? std::find(map[vec[i]].begin(), map[vec[i]].end(), vec[j]) == map[vec[i]].end() : cond;
+
+			for (int i = 0; i < vec.size() && !cond; i++)
+				for (int j = 0; j < i; j++)
+					if (std::find(map[vec[i]].begin(), map[vec[i]].end(), vec[j]) != map[vec[i]].end())
+					{
+						std::swap(vec[i], vec[j]);
+						i = j = 0;
+					}
+
+			if (!cond)
+				res += vec[vec.size() / 2];
+		}
+	}
+	std::cout << res << "\n";
+	return 0;
+}
